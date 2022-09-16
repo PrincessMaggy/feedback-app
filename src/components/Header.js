bb0fc03c7a135948a2693fdddaed8a9995fce95f
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-16 09:32:52 +0100

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header style={{backgroundColor:"rgba(0,0,0,0.4)", color:"#ff6a95"}}>
+      <h1> Feedback UI</h1>
+    </header>
+  )
+}
+
+
+
+export default Header;
