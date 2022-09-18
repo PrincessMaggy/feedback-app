dcf99f7b71b2cbcee89dbd1a8a8e714e9c030839
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-18 14:30:00 +0100

    conditional and prop drilling

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Card} from "../styles";
 
+import PropTypes from "prop-types";
+
 function FeedbackItem({item}) {
   return (
-    <Card>
+    <Card >
         <div className='num-display'> {item.rating} </div>
         <div className='text-display'> {item.text} </div>
     </Card>
   )
+}
+
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
