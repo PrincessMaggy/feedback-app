7c85ce04063c79ab5bb6e6f210729e8ad646ed9b
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-19 12:12:55 +0100

    added animation to feedback item

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 
+import {motion, AnimatePresence} from "framer-motion";
+
 import PropTypes from "prop-types"
 
 function FeedbackList({feedback, handleDelete}) {
@@ -11,12 +13,21 @@ function FeedbackList({feedback, handleDelete}) {
 
   return (
     <div className='feedback-list'>
+      <AnimatePresence>
         {feedback.map((item) => {
-            return <FeedbackItem
-                    key={item.id} 
-                    item={item}
-                    handleDelete ={handleDelete} />
+          
+            return <motion.div 
+                    key={item.id}
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
+                    exit={{opacity:0}}> 
+                      <FeedbackItem
+                        key={item.id} 
+                        item={item}
+                        handleDelete ={handleDelete} />
+                    </motion.div>
         })}
+        </AnimatePresence>
     </div>
   )
 }
