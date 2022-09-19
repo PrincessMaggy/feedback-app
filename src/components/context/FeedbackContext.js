3ce62255c650643a601f1392cb4b1fdf95770544
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-19 20:24:45 +0100

    update data to json server

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -55,12 +55,27 @@ export const FeedbackProvider = ({children}) =>{
         })
     }
 
-    const updateFeedback = (id, upItem) =>{
-        setFeedback(feedback.map((item) =>
-            item.id === id ? {
-                ...item, ...upItem
-            }: item ))
-    }
+    const updateFeedback = async (id, upItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(upItem),
+        })
+    
+        const data = await response.json()
+    
+        // NOTE: no need to spread data and item
+        setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+    
+        // FIX: this fixes being able to add a feedback after editing
+        // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+        setFeedbackEdit({
+          item: {},
+          edit: false,
+        })
+      }
 
     return <FeedbackContext.Provider
         value={{
